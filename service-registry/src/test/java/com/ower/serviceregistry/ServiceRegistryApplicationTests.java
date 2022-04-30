636620627b4e3439cf3a3de02deebda340da34c8
owermaiden <ower.maiden@cybertekschool.com>
2022-05-01 01:08:38 +0300

    service registry with eureka

---
+++ service-registry/src/test/java/com/ower/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ower.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
